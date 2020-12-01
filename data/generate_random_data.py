dfd96df90e3b3c866dc8998b6664c580fb20da60
lnbalon <lnbalon87@gmail.com>
2020-12-01 15:05:24 +0800

    minor edits

--- data/generate_random_data.py
+++ data/generate_random_data.py
@@ -8,11 +8,21 @@ def generate_random_dataframe(num_rows, seed=73):
     df['date'] = pd.date_range(start='2019-01-01', 
                                periods=num_rows,
                                freq='D')
+
     df['category1'] = [['AAA', 'BBB', 'CCC'][random.randrange(3)] for x in range(num_rows)]
     
     df['category2']  = [['XXXX', 'YYYY', 'ZZZZ', 'WWWW'][random.randrange(4)] for x in range(num_rows)]
-    
+     
+    df['category3']  = [['fjkdfd', 'qewert', 'fjlsdeiq', 'bacwep'][random.randrange(4)] for x in range(num_rows)]
+      
     df['value1'] = [random.randrange(10000, 12e5) for x in range(num_rows)]
+
+    df['value2'] = [random.randrange(80000, 11e5) for x in range(num_rows)]
+    
+    df['value3'] = [random.randrange(100000, 12e6) for x in range(num_rows)]
+
+    df['value4'] = [random.randrange(1, 10000)/10000 for x in range(num_rows)]
+
 
     return df
 
