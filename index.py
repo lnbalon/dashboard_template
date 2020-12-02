1fb5c41286338d4a28b8553f308809308e7cedfb
lnbalon <lnbalon87@gmail.com>
2020-12-02 23:38:12 +0800

    added chart 3

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import dash
 import flask
 import dash_html_components as html
-from layout import create_header, create_selector, create_summary, create_footer, create_chart1, create_chart2
+from layout import create_header, create_selector, create_summary, create_footer, \
+    create_chart1, create_chart2, create_chart3
 
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
@@ -12,6 +13,7 @@ selector = create_selector()
 footer = create_footer()
 chart1 = create_chart1()
 chart2 = create_chart2()
+chart3 = create_chart3()
 
 app.layout = html.Div(className='container',
                       children=[
