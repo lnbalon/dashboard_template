011950a190a6feb81390f29327217f0a3b4891f0
lnbalon <lnbalon87@gmail.com>
2020-12-03 23:02:37 +0800

    back to basics

--- layout.py
+++ layout.py
@@ -13,18 +13,7 @@ CATEGORY3 = sorted(get_category3(df))
 def create_header():
 
     header = html.Div(className='banner-container',
-                      children=[
-                          html.Img(
-                              className='banner-image',
-                              src='assets/logo.png'
-                          ),
-
-                          html.Div(
-                              className='banner-title',
-                              children=[html.H1(children=[APP_NAME])]
-                          )
-                      ]
-                      )
+                      children=['banner area'])
 
     return header
 
@@ -32,78 +21,7 @@ def create_header():
 def create_selector():
 
     selector = html.Div(className='selector-container',
-                        children=[
-
-                            # category1 selector
-                            html.Div(id='category1-selector',
-                                     children=[
-
-                                         html.Div('Select Category1:',
-                                                  style={'font-family': 'Arial',
-                                                         'font-size': '12px',
-                                                         'margin-top': '12px',
-                                                         'margin-left': '5px'}),
-
-                                         dcc.Dropdown(
-                                             className='dropdown',
-                                             id='categorySelector1',
-                                             options=[{'label': x, 'value': x} for x in CATEGORY1],
-                                             value=CATEGORY1[0],
-                                             style={'margin-top': '12px',
-                                                    'margin-bottom': '10px'}),
-
-                                         html.Div(style={'width': '90%',
-                                                         'margin': 'auto',
-                                                         'border-bottom': '1px solid black'})
-                                     ]),
-
-                            # category2 selector
-                            html.Div(id='category2-selector',
-                                     children=[
-
-                                         html.Div('Select Category2:',
-                                                  style={'font-family': 'Arial',
-                                                         'font-size': '12px',
-                                                         'margin-top': '2px',
-                                                         'margin-left': '5px'}),
-
-                                         dcc.Dropdown(
-                                             className='dropdown',
-                                             id='categorySelector2',
-                                             options=[{'label': x, 'value': x} for x in CATEGORY2],
-                                             value=CATEGORY2[0],
-                                             style={'margin-top': '12px',
-                                                    'margin-bottom': '10px'}),
-
-                                         html.Div(style={'width': '90%',
-                                                         'margin': 'auto',
-                                                         'border-bottom': '1px solid black'})
-                                     ]),
-
-                            # category2 selector
-                            html.Div(id='category3-selector',
-                                     children=[
-
-                                         html.Div('Select Category3:',
-                                                  style={'font-family': 'Arial',
-                                                         'font-size': '12px',
-                                                         'margin-top': '2px',
-                                                         'margin-left': '5px'}),
-
-                                         dcc.Dropdown(
-                                             className='dropdown',
-                                             id='categorySelector3',
-                                             options=[{'label': x, 'value': x} for x in CATEGORY3],
-                                             value=CATEGORY3[0],
-                                             style={'margin-top': '12px',
-                                                    'margin-bottom': '10px'}),
-
-                                         html.Div(style={'width': '90%',
-                                                         'margin': 'auto',
-                                                         'border-bottom': '1px solid black'})
-                                     ])
-
-                        ])
+                        children=['selector area'])
 
     return selector
 
@@ -111,14 +29,7 @@ def create_selector():
 def create_summary():
 
     summary = html.Div(className='summary-container',
-                       children=[
-
-                           html.Div(className='summary1',
-                                    children=[
-                                        html.H6('Category 1:'),
-                                        html.H6(id='category1')
-                                    ])
-                       ])
+                       children=['summary area'])
 
     return summary
 
