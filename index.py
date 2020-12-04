59d641bb1f24ce473dda63d2a8426e813767a453
lnbalon <lnbalon87@gmail.com>
2020-12-05 03:14:20 +0800

    added selector components

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 import dash
 import flask
 import dash_html_components as html
-from layout import logo_container, app_name_container, summary_container1, summary_container2
+from layout import logo_container, app_name_container, summary_container1, summary_container2, \
+    category1_selector_container, category2_selector_container
 
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
 
 logo = logo_container()
 app_name = app_name_container()
+selector1 = category1_selector_container()
+selector2 = category2_selector_container()
 summary1 = summary_container1()
 summary2 = summary_container2()
 
@@ -16,7 +19,9 @@ app.layout = html.Div(className='container',
                           logo,
                           app_name,
                           summary1,
-                          summary2
+                          summary2,
+                          selector1,
+                          selector2
                       ])
 
 if __name__ == '__main__':
--- layout.py
+++ layout.py
@@ -1,4 +1,5 @@
 import dash_html_components as html
+import dash_core_components as dcc
 from functions import get_category1, get_category2, get_category3
 import pandas as pd
 from config import PATH_DATASET1
@@ -26,6 +27,47 @@ def app_name_container():
                         children=['Sample Dashboard Template'])
 
     return app_name
+
+
+def category1_selector_container():
+
+    element = html.Div(className='category1-selector',
+                       children=[
+                           html.Div('Select Category',
+                                    style={'font': 'Helvetica',
+                                           'font-size': '12px',
+                                           'margin-bottom': '5px'}),
+
+                           dcc.Dropdown(id='demo-dropdown',
+                                        options=[{'label': x, 'value': x} for x in CATEGORY1],
+                                        value=CATEGORY1[0],
+                                        style={'font': 'Helvetica',
+                                               'font-size': '11px',
+                                               'width': '150px'})
+                       ])
+
+    return element
+
+
+def category2_selector_container():
+
+    element = html.Div(className='category2-selector',
+                       children=[
+                           html.Div('Select Category',
+                                    style={'font': 'Helvetica',
+                                           'font-size': '12px',
+                                           'margin-bottom': '5px'}),
+
+                           dcc.RadioItems(id='demo-checklist',
+                                          options=[{'label': x, 'value': x} for x in CATEGORY2],
+                                          value=CATEGORY2[0],
+                                          labelStyle={'display': 'block'},
+                                          style={'font': 'Helvetica',
+                                                 'font-size': '11px',
+                                                 'width': '150px'})
+                       ])
+
+    return element
 
 
 def summary_container1():
