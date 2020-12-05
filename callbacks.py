f6ea569ebdd48f2242dcb40744bf6d68a9d69b4d
lnbalon <lnbalon87@gmail.com>
2020-12-06 00:47:07 +0800

    inserted value1 callback

--- callbacks.py
+++ callbacks.py
@@ -1,4 +1,8 @@
 from dash.dependencies import Input, Output, State
+import pandas as pd
+from config import PATH_DATASET1
+
+df = pd.read_csv(PATH_DATASET1)
 
 
 def register_callbacks(app):
@@ -16,4 +20,17 @@ def register_callbacks(app):
         [Input('demo-checklist', 'value')])
     def output_category1(category2):
         return category2
+
+    # category2
+    @app.callback(
+        Output('value1', 'children'),
+        [Input('demo-dropdown', 'value'),
+         Input('demo-checklist', 'value')])
+    def output_value1(category1, category2):
+
+        df_new = df[(df['category1'] == category1) & (df['category2'] == category2)]
+        value1 = df_new['value1'].sum()
+
+        return value1
+
 
